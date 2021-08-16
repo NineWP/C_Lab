b8fe4abb07a65ab62e694bddb656822bfe419c26
Worrapon Rangsee <nine6805@gmail.com>
2021-08-16 23:17:58 +0700

    change c to w

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int main(){
-    int a, i, j, b=1, c;
+    int a, i, j, b=1, w;
     scanf("%d",&a);
-    c=(a*2)-1;
+    w=(a*2)-1;
     for(i=1;i<=a;i++){
-        for(j=1;j<=c;j++){
-            if(j<=b||j>c-b)
+        for(j=1;j<=w;j++){
+            if(j<=b||j>w-b)
                 printf("* ");
             else
                 printf("  ");
@@ -16,8 +16,8 @@ int main(){
     }
     b-=2;
     for(i=1;i<=a-1;i++){
-        for(j=1;j<=c;j++){
-            if(j<=b || j>c-b)
+        for(j=1;j<=w;j++){
+            if(j<=b || j>w-b)
                 printf("* ");
             else
                 printf("  ");
