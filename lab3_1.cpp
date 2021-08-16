fc6e2602d9eff0e1c6fe0f7a7afcaadb87543961
Worrapon Rangsee <nine6805@gmail.com>
2021-08-16 14:07:14 +0700

    lab3_1

---
+++ lab3_1.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void cal(int a){
+    int sum=0;
+    while(a!=0){
+        sum += a%10;
+        a=a/10;
+    }
+    if(sum>=10)
+        cal(sum);
+    else{
+        printf("%d",sum);
+    }
+}
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a<10)
+        printf("%d",a);
+    else
+        cal(a);
+}
