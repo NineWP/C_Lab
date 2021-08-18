2469cd951dcddf46f535eeb76a344b22792245bc
Worrapon Rangsee <nine6805@gmail.com>
2021-08-18 09:36:40 +0700

    week2_code1_first_commit

---
+++ week2_code1.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int a, i, j, b=0, w;
+    scanf("%d",&a);
+    w=(a*2)-1;
+    for(i=1;i<=a;i++){
+        for(j=1;j<=w;j++){
+            if(j>b && j<=w-b)
+                printf("* ");
+            else
+                printf("  ");
+        }
+    printf("\n");
+    b++;
+    }
+    b-=2;
+    for(i=1;i<=a-1;i++){
+        for(j=1;j<=w;j++){
+            if(j>b && j<=w-b)
+                printf("* ");
+            else
+                printf("  ");
+        }
+    printf("\n");
+    b--;
+    }
+}
