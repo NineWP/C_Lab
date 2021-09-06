7f7e8bfcbb744173e2f6dd16f44187457a66eaa9
NineWP <87454823+NineWP@users.noreply.github.com>
2021-09-06 11:24:10 +0700

    pointer study

---
+++ activity5_pointer.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+    char str[1000] , *p , k;
+    scanf("%s",str);
+    p = str;
+    for(int i = 0; *(p+i) != '\0'; i++)
+    {
+        if(i==0 || *(p+i) != *(p+i-1))
+        printf("%c", *(p+i));
+    }
+}
