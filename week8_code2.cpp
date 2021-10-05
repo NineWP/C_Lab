566fc52e36d5c5f51efec98c3983ae664b3767c4
NineWP <87454823+NineWP@users.noreply.github.com>
2021-10-05 20:10:49 +0700

    Swap with out third variable

--- week8_code2.cpp
+++ week8_code2.cpp
@@ -5,9 +5,9 @@ int main(){
     printf("Enter x and y : ");
     scanf("%d %d",&x, &y);
 
-    x = x*y;
-    y = x/y;
-    x = x/y;
+    x = x^y;
+    y = x^y;
+    x = x^y;
 
     printf("x = %d , y = %d", x, y);
 }
